5e86a367d3c84f7febef4884fd516db1e045375a
Tobias Johansen <tobias.p.johansen@outlook.com>
2024-05-14 16:07:24 +0200

    Added endpoint uploadsingle

--- app/main.py
+++ app/main.py
@@ -19,19 +19,67 @@ def allowed_image(filename):
     AllowedExtensions = {'jpg', 'jpeg'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in AllowedExtensions
 
-# POST /upload/?user_id=123
-@app.post("/V1/images/upload/")
-async def upload_image(user_id: int, file: UploadFile = File(...)):
+
+@app.post("/V1/images/uploadsingle/")
+async def upload_image_single(file: UploadFile = File(...)):
     # Check file extension
     if not allowed_image(file.filename):
         return {"error": "Only image files (jpg, jpeg) are allowed"}
+
+    # Define the directory where you want to save images
+    directory = "images"
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
     # Save image file
     with open(os.path.join("images", file.filename), "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     # Return Json response
-    return {"UserID": user_id, "filename": file.filename}
+    return {"filename": file.filename, "detection": True, "count": 3}
+
+# POST /upload/?user_id=123
+@app.post("/V1/images/upload/")
+async def upload_image(user_id: str, file: UploadFile = File(...)):
+    # Check file extension
+    if not allowed_image(file.filename):
+        return {"error": "Only image files (jpg, jpeg) are allowed"}
+
+    # Define the directory where you want to save images
+    directory = "images"
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Save image file
+    with open(os.path.join("images", file.filename), "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    # Return Json response
+    return {"UserID": user_id, "filename": file.filename, "detection": True, "count": 3}
+
+@app.post("/V1/images/cameraupload/")
+async def upload_image(user_id: str, camera_id: str, file: UploadFile = File(...)):
+    # Check file extension
+    if not allowed_image(file.filename):
+        return {"error": "Only image files (jpg, jpeg) are allowed"}
+
+    # Define the directory where you want to save images
+    directory = "images"
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Save image file
+    with open(os.path.join("images", file.filename), "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    # Return Json response
+    return {"UserID": user_id, "filename": file.filename, "camera_id": camera_id, "detection": True, "count": 3}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8006, host="0.0.0.0")
