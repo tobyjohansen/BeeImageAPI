ed2717b97c27e347c64077d8e3b0df4e22f6ef30
tobyjohansen <tobias.p.johansen@outlook.com>
2024-05-20 01:16:07 +0200

    Added MachineLearning modell to endpoint for detection of mite

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI,File, UploadFile
+from ultralytics import YOLO
+from fastapi.responses import JSONResponse
+import io
+import numpy as np
+from PIL import Image
 import shutil
 import os
 import uvicorn
 
 app = FastAPI()
 
+# Load Yolov8 model
+model = YOLO('mlmodel/best.pt')
 
 @app.get("/")
 async def root():
@@ -80,6 +87,55 @@ async def upload_image(user_id: str, camera_id: str, file: UploadFile = File(...
 
     # Return Json response
     return {"UserID": user_id, "filename": file.filename, "camera_id": camera_id, "detection": True, "count": 3}
+
+@app.post("/V1/images/uploadML/")
+async def upload_image(user_id: str, file: UploadFile = File(...)):
+    # Read image file
+    image_data = await file.read()
+    image = Image.open(io.BytesIO(image_data))
+
+    # Convert to numpy array
+    image_np = np.array(image)
+
+    # Perform prediction
+    results = model.predict(source=image_np)
+
+    count = 0
+    # Process results
+    for result in results:
+        count +=1
+
+    # Return Json response
+    return {"UserID": user_id, "filename": file.filename, "detection": True, "count": count}
+
+@app.post("/V1/images/uploadML2/")
+async def upload_image(user_id: str, file: UploadFile = File(...)):
+    # Check file extension
+    if not allowed_image(file.filename):
+        return {"error": "Only image files (jpg, jpeg) are allowed"}
+
+    # Define the directory where you want to save images
+    directory = "images"
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Save image file
+    file_path = os.path.join("images", file.filename)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    results = model(file_path)
+
+    count = 0
+    # Process results
+    for result in results:
+        for box in result.boxes:
+            count += 1
+
+    # Return Json response
+    return {"UserID": user_id, "filename": file.filename, "detection": True, "count": count}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8006, host="0.0.0.0")
